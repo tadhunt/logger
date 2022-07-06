c5b7c8e5bd72b3e4351f692dce14ae715438eae7
Tad Hunt <tadhunt@gmail.com>
2022-07-06 17:18:49 -0600

    initial version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tadhunt/logger
+
+go 1.18
---
+++ logger.go
@@ -0,0 +1,66 @@
+package logger
+
+// Modified from https://wycd.net/posts/2014-07-02-logging-function-names-in-go.html
+
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"reflect"
+)
+
+var Log = log.New(LogWriter{}, "", 0)
+
+type LogWriter struct{}
+
+func (f LogWriter) Write(p []byte) (n int, err error) {
+	pc, file, line, ok := runtime.Caller(3)
+	if !ok {
+		file = "?"
+		line = 0
+	}
+
+	fn := runtime.FuncForPC(pc)
+	var fnName string
+	if fn == nil {
+		fnName = "?()"
+	} else {
+		dotName := filepath.Ext(fn.Name())
+		fnName = strings.TrimLeft(dotName, ".") + "()"
+	}
+
+	log.Printf("%s:%d %s: %s", filepath.Base(file), line, fnName, p)
+	return len(p), nil
+}
+
+func FuncInfo(f interface{}) string {
+	fn := runtime.FuncForPC(reflect.ValueOf(f).Pointer())
+
+	var fnName string
+	if fn == nil {
+		fnName = "?()"
+	} else {
+		dotName := filepath.Ext(fn.Name())
+		fnName = strings.TrimLeft(dotName, ".") + "()"
+	}
+
+	file, line := fn.FileLine(fn.Entry())
+	return fmt.Sprintf("%s:%d %s", filepath.Base(file), line, fnName)
+}
+
+func Caller() string {
+
+	pc, _, _, ok := runtime.Caller(2)
+	if !ok {
+		return "<unknown>"
+	}
+
+	pcs := []uintptr{pc}
+
+	frames := runtime.CallersFrames(pcs)
+	frame, _ := frames.Next()
+
+	return fmt.Sprintf("%s:%d %s", frame.File, frame.Line, frame.Function)
+}
